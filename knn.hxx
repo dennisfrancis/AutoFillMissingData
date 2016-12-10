c65ab2379cc362c689977eb741aaf643eb03e319
Dennis Francis <dennisfrancis.in@gmail.com>
2016-12-10 11:13:58 +0530

    cleaned up a couple of unused functions in knn.hxx

--- knn.hxx
+++ knn.hxx
@@ -48,52 +48,6 @@ double getScore( const Sequence< Sequence< Any > >& rDataArray,
 		 const std::vector<DataType>& rColType,
 		 const std::vector< Any >& rPreds );
 
-double pround(double fX, sal_Int32 nPrecision)
-{
-    if (fX == 0.)
-        return fX;
-    sal_Int32 nEx = floor(log10(abs(fX))) - nPrecision + 1;
-    double fDiv = pow(10, nEx);
-    return floor(fX / fDiv + 0.5) * fDiv;
-}
-
-sal_Int32 getPrecision(double fVal)
-{
-    char aStr[100];
-    sprintf( aStr, "%.15f", fVal);
-    bool bDecimal = false;
-    sal_Int32 nPrecision = 0;
-    for ( sal_Int32 nIdx = 0; aStr[nIdx]; ++nIdx )
-    {
-        if ( aStr[nIdx] == '.' )
-        {
-            bDecimal = true;
-            continue;
-        }
-
-        if( bDecimal )
-        {
-            ++nPrecision;
-            if ( aStr[nIdx] == '0' )
-            {
-                bool bFound = true;
-                for ( sal_Int32 nIdx2 = nIdx+1; aStr[nIdx] && nIdx2 < 4; ++nIdx2 )
-                {
-                    if ( aStr[nIdx2] != '0' )
-                    {
-                        bFound = false;
-                        break;
-                    }
-                }
-                if ( bFound )
-                    return nPrecision - 1;
-            }
-        }
-    }
-
-    return nPrecision - 1;
-}
-
 void computeMissingValuesInColumnKNN( Sequence< Sequence< Any > >& rDataArray,
 				      const sal_Int32 nLabelIdx,
 				      const std::unordered_set<sal_Int32>& rTestRowIndicesSet,
@@ -219,10 +173,6 @@ void fitPredict( const sal_Int32 nKparam,
 		double fCandTarget;
 		// Read the label/target of the candidate train point
 		rDataArray[rDistancesMatrix[nTargetIdx][nCandIdx].second][nLabelIdx] >>= fCandTarget;
-		/*sal_Int32 nCandPrec = getPrecision( fCandTarget );
-		if ( nCandPrec > nPrecision )
-		    nPrecision = nCandPrec;
-		*/
 
 		//printf("DEBUG>>> Testidx = %d : Cand %d has fSimilarity = %.6f, fCandTarget = %.4f\n", nTestIdx, nCandIdx, fSimilarity, fCandTarget); fflush(stdout);
 		fTarget += ( fSimilarity * fCandTarget );
@@ -231,7 +181,6 @@ void fitPredict( const sal_Int32 nKparam,
 	    // Find the weighted target for the test point.
 	    fTarget = fTarget / fScale;
 	    //printf("DEBUG>>> Testidx = %d : found target = %.6f, scale = %.6f precision = %d\n", nTestIdx, fTarget, fScale, nPrecision); fflush(stdout);
-	    //fTarget = pround( fTarget, nPrecision );
 	    rTargets[nTargetIdx] <<= fTarget;
 	}
 	
